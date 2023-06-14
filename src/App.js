7f05fa54aaafcd8702a991220c3d63a7723b3655
arshan_alee <mirarshanali@gmail.com>
2023-06-15 03:25:53 +0500

    condition added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import "./App.css";
-import axios from "axios";
-function App() {
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
+function App() {
   // Retrieve the totalAmount parameter from the URL
   const urlParams = new URLSearchParams(window.location.search);
-  const amount = urlParams.get('amount');
+  let amount = urlParams.get('amount');
+
+  // Set a default price if amount is not received
+  if (!amount) {
+    amount = '100'; // Default price value
+  }
 
   // Display the totalAmount on the webpage
-  document.getElementById("totalAmount").textContent = amount;
+  useEffect(() => {
+    document.getElementById('totalAmount').textContent = amount + " $";
+  }, [amount]);
 
   const handlePayment = async () => {
-    const payment = await axios.post("https://cautious-sundress-hen.cyclic.app/stripe-checkout", {
-      amount,
-    });
+    const payment = await axios.post(
+      'https://cautious-sundress-hen.cyclic.app/stripe-checkout',
+      {
+        amount,
+      }
+    );
 
     const paymentUrl = payment.data.url;
 
     // Open the payment URL on the same page
     window.location.href = paymentUrl;
   };
+
   return (
-    <div class="container">
+    <div className="container">
       <div id="totalAmountContainer">
         <img src="./Khariidar-Logo.png" alt="Total Amount Image" id="totalAmountImage" />
       </div>
-      <h1 class="title">Total Order Amount:</h1>
+      <h1 className="title">Total Order Amount:</h1>
       <p id="totalAmount"></p>
       <h3 id="greetings">Thank you for Shopping!</h3>
-      <button id="submitButton" onclick={handlePayment}>Place Order</button>
+      <button id="submitButton" onClick={handlePayment}>
+        Place Order
+      </button>
     </div>
   );
 }
